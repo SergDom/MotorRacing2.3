95ce0530754ff3a5dd39f02de61bd19e7e6b4308
sergeydomanov <nomad-labs@yandex.ru>
2022-10-19 20:22:14 +0300

    Sets collections

--- src/ProductList/RandomList.java
+++ src/ProductList/RandomList.java
@@ -4,30 +4,31 @@ import java.util.*;
 
 public class RandomList {
 
-    private static final Random fillObject = new Random();
 
     public static void main(String[] args) {
 
-        Queue<Integer> randomlist = new ArrayDeque<>(20);
+        ArrayList<Integer> randomlist = new ArrayList<>(20);
 
 
         fillArray(randomlist);
-        System.out.println(randomlist);
     }
 
-    private static void fillArray(Queue<Integer> randomlist) {
-        int size = fillObject.nextInt(1000);
-        for (int i = 0; i < size ; i++) {
-//            int uneven = randomlist.get(i) % 2;
-//            if (uneven != 0) {
-//                randomlist.remove(i);
-//            } else {
-                randomlist.offer(fillObject.nextInt(randomlist.size()));
+    private static void fillArray(ArrayList<Integer> randomlist) {
 
-//                queue.offer(customers.get(fillObject.nextInt(customers.size())));
+        for (int i = 0; i < 20; i++) {
+            randomlist.add(i, (int) (Math.random()*1000));
+        }
+        for (int i = 0; i < randomlist.size(); i++) {
+            if (randomlist.get(i) % 2 == 0) {
+                System.out.println(randomlist.get(i));
+            } else {
+                randomlist.remove(i);
             }
         }
     }
+
+}
+
 
 
 
