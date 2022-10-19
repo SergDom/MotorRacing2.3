3f3291c19786b8ba368d969146039051c9d57e9d
sergeydomanov <nomad-labs@yandex.ru>
2022-10-19 21:52:17 +0300

    Sets collections

--- src/ProductList/Main.java
+++ src/ProductList/Main.java
@@ -1,6 +1,6 @@
 package ProductList;
 
-public class Shopping {
+public class Main {
 
     public static void main(String[] args) {
 
---
+++ src/ProductList/Recipes.java
@@ -0,0 +1,10 @@
+package ProductList;
+
+import java.util.Set;
+
+public class Recipes {
+    private String name;
+    private double totalSum;
+    private Set <Product> products;
+
+}
